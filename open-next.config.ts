6cb90958f2954852c980041851cfeaabedb6f337
jeganxthan <jegan4044@gmail.com>
2025-12-29 12:00:52 +0530

    vercel deploy 2

--- open-next.config.ts
+++
@@ -1,6 +0,0 @@
-// open-next.config.ts
-import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-
-export default defineCloudflareConfig({
-  // ✅ Disable ISR / R2 completely (FREE)
-});
